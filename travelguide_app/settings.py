36e1982e7a13ff3b3a322fcf01c9c50452484ffd
Paulo Victor Gomes <pv.gomes89@gmail.com>
2024-07-05 08:36:31 +0200

    again

--- travelguide_app/settings.py
+++ travelguide_app/settings.py
@@ -121,12 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'staticfiles/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles")
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
