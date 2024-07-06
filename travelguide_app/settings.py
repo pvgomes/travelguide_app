3d397a5a50be1213902d68f26bd83edaffb94fa0
Paulo Victor Gomes <pv.gomes89@gmail.com>
2024-07-06 13:35:32 +0200

    add root file

--- travelguide_app/settings.py
+++ travelguide_app/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles" 
+
 STATIC_URL = 'staticfiles/'
-#STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
